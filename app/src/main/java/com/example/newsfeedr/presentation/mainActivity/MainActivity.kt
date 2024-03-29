9a362003baad8b9b69eb08474eff98c34de2bf4c
mahmoud <mahmoud.aboelhamd@onecard.net>
2024-03-29 16:49:22 +0200

    customize system bars color

--- app/src/main/java/com/example/newsfeedr/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/newsfeedr/presentation/mainActivity/MainActivity.kt
@@ -5,14 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.newsfeedr.domain.usecases.app_entry.AppEntryUseCases
 import com.example.newsfeedr.presentation.navgraph.NavGraph
 import com.example.newsfeedr.ui.theme.NewsFeedrTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -29,7 +34,15 @@ class MainActivity : ComponentActivity() {
             NewsFeedrTheme(
                 dynamicColor = false
             ) {
-                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()) {
                     NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
